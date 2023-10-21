879a2f9b405b81c18e738dc0065cd2cc55349610
wildemberg-sales <junior_sales2010@hotmail.com>
2023-10-21 19:28:23 -0300

    Subindo arquivos do site

---
+++ web/index.js
@@ -0,0 +1,9 @@
+// const swiperEl = document.querySelector('swiper-container');
+
+//   swiperEl.addEventListener('progress', (event) => {
+//     const [swiper, progress] = event.detail;
+//   });
+
+//   swiperEl.addEventListener('slidechange', (event) => {
+//     console.log('slide changed');
+//   });
