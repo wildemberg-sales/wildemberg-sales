ea22454ce479c86e93ae8bf0d304cb1c577fbe47
wildemberg-sales <junior_sales2010@hotmail.com>
2023-10-22 20:30:57 -0300

    Subindo versão mobile

--- web/index.js
+++
@@ -1,9 +0,0 @@
-// const swiperEl = document.querySelector('swiper-container');
-
-//   swiperEl.addEventListener('progress', (event) => {
-//     const [swiper, progress] = event.detail;
-//   });
-
-//   swiperEl.addEventListener('slidechange', (event) => {
-//     console.log('slide changed');
-//   });
